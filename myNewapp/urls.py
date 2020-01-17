05fc22f82fa4231b6c8cbd4c6a50b8a861353faf
steffytw <steffythankamwilson@cs.ajce.in>
2020-01-17 11:05:26 +0530

    eigth commit

--- myNewapp/urls.py
+++ myNewapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
+    path('display/',views.display,name = 'display'),
 ]
--- myNewapp/views.py
+++ myNewapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import  student
 
 # Create your views here.
 
 def home(request):
 	context = "HELLO EVERYONE"
 	return render(request,'myNewapp/home.html',{'content':context})
+
+def display(request):
+	students = student.objects.all[:5]
+	return render(request,'display.html',{'students':student})
+
+
 	
